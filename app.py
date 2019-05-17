c7e8b4c4092abc42c7d126709dc8d5d354985aac
Ankith M <reachankit2013@gmail.com>
2019-05-17 15:25:35 +0530

    Test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.static_folder = 'static'
 # compress = FlaskStaticCompress(app)
 
 
-prog = ClarifaiApp(api_key='<ADD YOUR API KEY>') # Use the api key provided by clarify for your application
+prog = ClarifaiApp(api_key='c02ed0679e544b64b2ec5091786634b6') # Use the api key provided by clarify for your application
 
 UPLOAD_FOLDER = os.getcwd() + '/uploads'
 
@@ -44,4 +44,5 @@ def result():
             return jsonify({"recognised" :False, "payload":None})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" , port = 3000, debug = True)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
