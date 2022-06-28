42860b32f3b57859a8daa75e9134e626c41a053c
Ankith M <reachankit2013@gmail.com>
2022-06-28 20:04:35 +0800

    Update app.py to gRPC

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def result():
             return jsonify({"recognised" :False, "payload":None})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 3000), debug=True)
